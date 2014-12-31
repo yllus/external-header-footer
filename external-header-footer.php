121eb5fd84f1131531e2d3abc16a31d4068ca65a
Sully Syed <sully@yllus.com>
2014-12-31 18:13:53 -0500

    Clean up plug-in.

--- external-header-footer.php
+++ external-header-footer.php
@@ -1,12 +1,66 @@
 <?php
 /*
  Plugin Name: External Header Footer
- Plugin URI: https://github.com/digitalmedia/external-header-footer
+ Plugin URI: https://github.com/yllus/external-header-footer
  Description: Exposes the header and footer of the website as individual files, allowing for external consumption (for third parties sites that want a similar design style).
  Author: Sully Syed
  Version: 1.0
  Author URI: http://yllus.com/
 */
+
+// Add a new rewrite rule that points to our exposed header and footer.
+function ehf_add_rewrite_rules_parameters() {
+	add_rewrite_tag('%ehf_template%','([^&]+)');
+
+	add_rewrite_rule( '^external-header-footer/([header|footer|demo]+)/?$', 'index.php?ehf_template=$matches[1]', 'top');
+}
+add_action('init', 'ehf_add_rewrite_rules_parameters', 1);
+
+// Handle requests that contain the "ehf_template" parameter.
+function ehf_parse_request( &$wp ) {
+	if ( array_key_exists( 'ehf_template', $wp->query_vars ) ) {
+        global $wp_query;
+
+        // If we've disallowed output, exit immediately.
+		if ( ( (int) get_option('ehf_expose_header_and_footer', 0) ) == 0 )  {
+			return;
+		}
+
+        switch ( $wp->query_vars['ehf_template'] ) {
+        	case 'header':
+        		// Execute any actions that have been coded into the theme/other plug-ins to run before the footer is output.
+				do_action('external_header_footer_pre_header');
+
+				// Output the header of the website.
+				get_header();
+				exit;
+        		break;
+        	case 'footer':
+    			// Execute any actions that have been coded into the theme/other plug-ins to run before the footer is output.
+				do_action('external_header_footer_pre_footer');
+
+				// Output the footer of the website.
+				get_footer(); 
+				exit;
+				break;
+			case 'demo':
+				// Output the external header to the page.
+				ehf_output_external_header();
+				?>
+
+				<p>
+				Right here is where content is displayed; you should see the header and footer of the external website above and below this text.
+				</p>
+
+				<?php 
+				// Output the external footer to the page.
+				ehf_output_external_footer();
+				exit;
+				break;
+        }
+    }
+}
+add_action('parse_request', 'ehf_parse_request');
 
 /**
  * Output the contents of the external header wherever the function ehf_output_external_header() is called.
@@ -106,12 +160,12 @@ function external_header_footer_do_settings_page() {
  */
 function external_header_footer_init() {
     // Add the settings section that all of our fields will belong to (heading not shown).
-    add_settings_section('external_header_footer_settings_section', '', 'external_header_footer_settings_section_text', 'external_header_footer_settings_page');
+    add_settings_section('external_header_footer_settings_section', '', 'ehf_header_footer_settings_section_text', 'external_header_footer_settings_page');
 
     // Add the "Expose Header and Footer" field (blank title here, output in its function), registered to the group "external_header_footer_settings_group", and 
     // output in the function ehf_expose_header_and_footer_checkbox().
     add_settings_field('ehf_expose_header_and_footer', '', 'ehf_expose_header_and_footer_checkbox', 'external_header_footer_settings_page', 'external_header_footer_settings_section');
-    register_setting('external_header_footer_settings_group', 'ehf_expose_header_and_footer');
+    register_setting('external_header_footer_settings_group', 'ehf_expose_header_and_footer', 'ehf_flush_rewrite_rules');
 
     // Add the "External Header URL" field (blank title here, output in its function), registered to the group "external_header_footer_settings_group", output in 
     // the function ext_external_header_url_text() and using the sanitizing function of ehf_external_clear_cache().
@@ -130,6 +184,10 @@ function external_header_footer_init() {
 }
 add_action('admin_init', 'external_header_footer_init');
 
+function ehf_header_footer_settings_section_text() {
+	1;
+}
+
 function ehf_expose_header_and_footer_checkbox() {
 	// Retrieve and expose the "Expose Header and Footer" setting.
 	$ehf_expose_header_and_footer = (int) get_option('ehf_expose_header_and_footer', 0);
@@ -139,13 +197,22 @@ function ehf_expose_header_and_footer_checkbox() {
 	}
 
 	// Retrieve the URLs for the header, footer and test page.
-	$ehf_header_url = plugins_url('external-header-footer/header.php');
-	$ehf_footer_url = plugins_url('external-header-footer/footer.php');
+	$ehf_header_url = home_url('/external-header-footer/header/');
+	$ehf_footer_url = home_url('/external-header-footer/footer/');
 	$ehf_test_url = plugins_url('external-header-footer/test-page.php');
 	?>
 	<tr valign="top">
 		<th colspan="2">
 			<h3>Expose Header for External Sites</h3>
+			<p style="font-weight: normal;">
+				If you're got an external website that you'd like to dress up with the same header and footer as this WordPress site, check the <b>Expose Header and Footer</b> 
+				option below, and run a script on that external site to pull down the contents of the <b>Header URL</b> and <b>Footer URL</b> on a regular basis to keep the 
+				two site looking the same.
+			</p>
+			<p style="font-weight: normal;">
+				Once you've checked <b>Expose Header and Footer</b> and pressed the <b>Save Changes</b> button to enable the option, check out the page at <b>Demo Page URL</b>, 
+				and take a look at its source code to see an example of how to retrieve and display the header and footer of this website using PHP.
+			</p>
 		</th>
 	</tr>
 
@@ -220,6 +287,14 @@ function ext_external_header_url_text() {
 	<tr valign="top">
 		<th colspan="2">
 			<h3>Consume Header / Footer from External Website</h3>
+			<p style="font-weight: normal;">
+				If you've enabled the External Header Footer plug-in on another WordPress website, and want to use its header on <i>this</i> WordPress website, you can use the 
+				fields below to automatically retrieve the header and footer of that website. 
+			</p>
+			<p style="font-weight: normal;">
+				Next, update the <code>header.php</code> and <code>footer.php</code> files of this WordPress theme to call the function <code>ehf_output_external_header()</code> 
+				and <code>ehf_output_external_footer()</code> respectively. This plug-in will automatically retrieve and cache the contents of the external site's header and 
+				footer for the amount of minutes specified in <b>Cache Header/Footer For</b>.
 		</th>
 	</tr>
 
@@ -227,7 +302,7 @@ function ext_external_header_url_text() {
 		<th scope="row"><label for="ehf_external_header_url">External Header URL</label></th>
 		<td>
 			<input name="ehf_external_header_url" type="text" id="ehf_external_header_url" value="<?php echo $ehf_external_header_url; ?>" class="regular-text code" style="width: 600px;" />
-			<p class="description">If filled out, the <code>ehf_output_external_header()</code> function will output the contents of the page retrieved at this URL.</p>
+			<p class="description">The function <code>ehf_output_external_header()</code> will output the contents of the page retrieved at the URL input into the field above.</p>
 		</td>
 	</tr>
 	<?php
@@ -241,7 +316,7 @@ function ext_external_footer_url_text() {
 		<th scope="row"><label for="ehf_external_footer_url">External Footer URL</label></th>
 		<td>
 			<input name="ehf_external_footer_url" type="text" id="ehf_external_footer_url" value="<?php echo $ehf_external_footer_url; ?>" class="regular-text code" style="width: 600px;" />
-			<p class="description">If filled out, the <code>ehf_output_external_footer()</code> function will output the contents of the page retrieved at this URL.</p>
+			<p class="description">The function <code>ehf_output_external_footer()</code> will output the contents of the page retrieved at the URL input into the field above.</p>
 		</td>
 	</tr>		
 	<?php
@@ -252,7 +327,7 @@ function ehf_external_cache_expiry_text() {
 	$ehf_external_cache_expiry = get_option('ehf_external_cache_expiry', '60');
 
 	// Retrieve the URLs for the external test page.
-	$ehf_external_test_url = plugins_url('external-header-footer/test-page-external.php');
+	$ehf_external_test_url = home_url('/external-header-footer/demo/');
 	?>
 	<tr valign="top">
 		<th scope="row"><label for="ehf_external_cache_expiry">Cache Header/Footer For</label></th>
@@ -275,14 +350,28 @@ function ehf_external_cache_expiry_text() {
 }
 
 /**
- * Called when a new value is sent to the "External Header URL" or "External Footer URL"; clears the Transients API cache of 
- * what may already be saved to those fields to ensure changes to what is wished to be retrieved occurs immediately.
+ * Called when a new value is sent to the "Expose Header and Footer" field; flushes the internal cache of WordPress rewrite rules / permalinks 
+ * to ensure the new rules for the plug-in are accessible.
  *
- * @return void
+ * @return string
  */
 function ehf_external_clear_cache( $value ) {
 	delete_transient('ehf_external_header_url');
 	delete_transient('ehf_external_footer_url');
+
+	return $value;
+}
+
+/**
+ * Called when a new value is sent to the "External Header URL" or "External Footer URL"; clears the Transients API cache of 
+ * what may already be saved to those fields to ensure changes to what is wished to be retrieved occurs immediately.
+ *
+ * @return integer
+ */
+function ehf_flush_rewrite_rules( $value ) {
+	global $wp_rewrite;
+
+	$wp_rewrite->flush_rules( false );
 
 	return $value;
 }
--- test-page.php
+++ test-page.php
@@ -1,16 +1,38 @@
 <?php
-// Load in order to access standard WordPress functions.
-require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
+/* This file consists of a demonstration script for how to retrieve the header and footer of a WordPress site 
+ * that has the External Header Footer plug-in enabled. It's meant to be completely standalone of WordPress 
+ * itself, and uses pure PHP functions only.
+ *
+ * If you want to take the header/footer of one WordPress site and use it on a second WordPress site, look 
+ * at the "Consume Header / Footer from External Website" section of the plug-in's Settings page instead.
+ */
 
-// Set the URLs for the header and footer files.
-$str_header_url = plugins_url() . '/external-header-footer/header.php';
-$str_footer_url = plugins_url() . '/external-header-footer/footer.php';
+// Take a guess at what the URLs to the WordPress site's exposed header and footer are...
+$str_header_url = $_SERVER['SERVER_NAME'] . '/external-header-footer/header/';
+$str_footer_url = $_SERVER['SERVER_NAME'] . '/external-header-footer/footer/';
 
-// Output the header to the page.
-$arr_header = wp_remote_get($str_header_url);
-echo $arr_header['body'];
+// Define a function that uses the cURL library to retrieve the contents of a URL and returns it in a string.
+function curl_download( $url ) {
+    if ( !function_exists('curl_init') ) {
+        die('cURL is not installed. Install and try again.');
+    }
+  
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+  
+    return $output;
+}
 ?>
 
+<?php
+// Retrieve and output the header to the page.
+echo curl_download($str_header_url);
+?>
+
+<!-- Output some generic HTML text; this is the middle area between the consumed header and footer where you'd display site content. -->
 <p>
 Right here is where content is displayed, wrapped by the header and footer of this website.
 <br /><br />
@@ -22,7 +44,6 @@ Thanks for using the <b>External Header Footer</b> plug-in! Please write to <a h
 </p>
 
 <?php 
-// Output the footer to the page.
-$arr_footer = wp_remote_get($str_footer_url);
-echo $arr_footer['body'];
+// Retrieve and output the footer to the page.
+echo curl_download($str_footer_url);
 ?>
